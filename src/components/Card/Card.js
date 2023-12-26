5523d8998c40b39a5d9ebc659f2a65fca74f5c02
Bruno <bruno.dill@protonmail.com>
2023-12-26 11:35:41 +0100

    made card button funtional

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,14 @@
 import "./Card.css";
 import { BookmarkIcon } from "../../assets/bookmark-icon";
+import { useState } from "react";
 
 export default function Card() {
+  const [answerHidden, setAnswerHidden] = useState(true);
+
+  function showAnswer() {
+    setAnswerHidden(!answerHidden);
+  }
+
   return (
     <article className="question-container">
       <input
@@ -13,8 +20,10 @@ export default function Card() {
         <BookmarkIcon className="bookmark" />
       </label>
       <h2 className="question">Question</h2>
-      <button className="answer-button">Show Answer</button>
-      <p className="answer">Answer</p>
+      <button className="answer-button" onClick={showAnswer}>
+        {answerHidden ? "Show" : "Hide"} Answer
+      </button>
+      <p className={answerHidden ? "answer hidden" : "answer"}>Answer</p>
       <ul className="tags" aria-label="question tags">
         <li className="tag">1</li>
         <li className="tag">2</li>
